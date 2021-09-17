38d6da658c3b2a3d999d14c4138e05b0dd8924f3
nforne <wnforne@gmail.com>
2021-09-17 21:58:55 +0000

    ice-cream.js, packing-list.js, etc

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -13,3 +13,38 @@ for (let i = 100; i <= 200; i++) {
 // for (let i = 100; i <= 200; i++) {
 //     console.log(i);
 //   }
+
+// traditional for loops
+const amounts = [61.00, 52.25, 112.99, 5.00]; 
+const total = 0;
+for (let i = 0; i < amounts.length; i++) {
+  total += amounts[i];
+}
+console.log('Order total is: ', total);
+// newer for...of loops - simpler
+
+/*const total = 0;
+for (let amount of amounts) {
+  total += amount;
+}
+console.log('Order total is: ', total);*/
+
+const total = 0;
+for (let i of amounts) {
+  total += i;
+}
+console.log('Order total is: ', total);
+
+// -----------------------------------------------
+
+function buildTriangle(num) {
+    let str = '';   //to store triangle in one string
+    let t = ''      // to accumulate * strings
+    for (let i = 1; i <= num; i++) {
+        t += '*'
+        str += t + '\n';
+    }
+    str.slice(0, str.length - 2);
+    return str
+}
+console.log(buildTriangle(10));
