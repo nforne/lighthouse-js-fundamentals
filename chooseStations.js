f41c224a89bf99420023e079d183eb347a08eb4b
nforne <wnforne@gmail.com>
2021-09-17 11:54:41 +0000

    added function chooseStations

--- chooseStations.js
+++ chooseStations.js
@@ -4,7 +4,7 @@ function chooseStations(stations) {
   let goodStations = [];
   for (let i = 0; i < stations.length; i++) {
     if (stations[i][1] >= 20 && (stations[i][2] === 'school' || stations[i][2] === 'community centre')) {
-      goodStations.push(stations[i][0])
+      goodStations.push(stations[i][0]);
     }
   };
   return goodStations;
