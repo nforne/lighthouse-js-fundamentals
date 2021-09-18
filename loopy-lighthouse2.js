2da5a1290cc1a1035e11548323ab64f9a040369d
nforne <wnforne@gmail.com>
2021-09-18 02:04:06 +0000

    added loopyLighthouse2.js

--- loopy-lighthouse2.js
+++ loopy-lighthouse2.js
@@ -1,6 +1,8 @@
 function loopyLighthouse(range, multiples, words) {
   for (let i = range[0]; i <= range[1]; i++) {
-    if (i % multiples[0] === 0) {
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+      console.log("BattyBeacon")
+    } else if (i % multiples[0] === 0) {
       console.log(words[0]);
     } else if (i % multiples[1] === 0) {
       console.log(words[1]);
